94fcdaa0325e048c2e6deb541751987031b8568a
qide <qide.yi@castbox.fm>
2022-01-19 16:19:53 +0800

    添加加密工具

--- Assets/Script/Tools/OtherTools.cs
+++ Assets/Script/Tools/OtherTools.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
 
 /// <summary>
 /// 其它工具
 /// </summary>
-public static class OtherTools 
+public static class OtherTools
 {
     /// <summary>
     /// 通过反射来获取字段
@@ -16,55 +19,57 @@ public static class OtherTools
     /// <param name="GetBase">是否要获取到父类和接口的字段</param>
     /// <typeparam name="T"></typeparam>
     /// <typeparam name="T2"></typeparam>
-    public static void GetField<T,T2>(ref T Class ,string FieldName,out T2 vale,bool GetBase=true)
+    public static void GetField<T, T2>(ref T Class, string FieldName, out T2 vale, bool GetBase = true)
     {
-        Type type= Class.GetType();
+        Type type = Class.GetType();
         List<FieldInfo> fileInfos = new List<FieldInfo>();
         if (GetBase)
         {
             while (true)
             {
-                var baseType =type.BaseType;
-                if (baseType==typeof(object))
+                var baseType = type.BaseType;
+                if (baseType == typeof(object))
                 {
                     break;
                 }
-                var Interface= type.GetInterfaces();
-                var s= type.GetFields(
-                    BindingFlags.Instance| 
-                    BindingFlags.NonPublic|
+
+                var Interface = type.GetInterfaces();
+                var s = type.GetFields(
+                    BindingFlags.Instance |
+                    BindingFlags.NonPublic |
                     BindingFlags.Default |
-                    BindingFlags.Public | 
+                    BindingFlags.Public |
                     BindingFlags.Static);
                 fileInfos.AddRange(s);
                 foreach (var item in Interface)
                 {
-                    var i= item.GetFields(BindingFlags.Instance| 
-                                          BindingFlags.NonPublic|
-                                          BindingFlags.Default |
-                                          BindingFlags.Public | 
-                                          BindingFlags.Static);
+                    var i = item.GetFields(BindingFlags.Instance |
+                                           BindingFlags.NonPublic |
+                                           BindingFlags.Default |
+                                           BindingFlags.Public |
+                                           BindingFlags.Static);
                     fileInfos.AddRange(i);
                 }
+
                 type = baseType;
             }
 
             foreach (var item in fileInfos)
             {
-                if (item.Name==FieldName)
+                if (item.Name == FieldName)
                 {
-                    vale=(T2)item.GetValue(Class);
+                    vale = (T2) item.GetValue(Class);
                     return;
                 }
             }
         }
         else
         {
-            var s= type.GetFields(
-                BindingFlags.Instance| 
-                BindingFlags.NonPublic|
+            var s = type.GetFields(
+                BindingFlags.Instance |
+                BindingFlags.NonPublic |
                 BindingFlags.Default |
-                BindingFlags.Public | 
+                BindingFlags.Public |
                 BindingFlags.Static);
             fileInfos.AddRange(s);
             foreach (var item in fileInfos)
@@ -77,6 +82,71 @@ public static class OtherTools
             }
         }
 
-        vale=default;
+        vale = default;
     }
+
+    #region 加密工具
+
+    /// <summary>
+    /// 默认密钥向量
+    /// </summary>
+    private static byte[] Keys = {0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF};
+
+    /// <summary>
+    /// DES加密字符串
+    /// </summary>
+    /// <param name="encryptString">待加密的字符串</param>
+    /// <param name="encryptKey">加密密钥,要求为8位(多余的只取前8位，不能为中文)</param>
+    /// <returns>加密成功返回加密后的字符串，失败返回源串</returns>
+    public static string EncryptDES(string encryptString, string encryptKey)
+    {
+        try
+        {
+            byte[] rgbKey = Encoding.UTF8.GetBytes(encryptKey.Substring(0, 8));
+            //byte[] rgbIV = Keys;
+            byte[] inputByteArray = Encoding.UTF8.GetBytes(encryptString);
+            DESCryptoServiceProvider dCSP = new DESCryptoServiceProvider();
+            MemoryStream mStream = new MemoryStream();
+            CryptoStream cStream = new CryptoStream(mStream, dCSP.CreateEncryptor(rgbKey, Keys), CryptoStreamMode.Write);
+            cStream.Write(inputByteArray, 0, inputByteArray.Length);
+            cStream.FlushFinalBlock();
+            cStream.Dispose();
+            mStream.Dispose();
+            return Convert.ToBase64String(mStream.ToArray());
+        }
+        catch
+        {
+            return encryptString;
+        }
+    }
+
+    /// <summary>
+    /// DES解密字符串
+    /// </summary>
+    /// <param name="decryptString">待解密的字符串</param>
+    /// <param name="decryptKey">解密密钥,要求为8位,和加密密钥相同</param>
+    /// <returns>解密成功返回解密后的字符串，失败返源串</returns>
+    public static string DecryptDES(string decryptString, string decryptKey)
+    {
+        try
+        {
+            byte[] rgbKey = Encoding.UTF8.GetBytes(decryptKey);
+            //byte[] rgbIV = Keys;
+            byte[] inputByteArray = Convert.FromBase64String(decryptString);
+            DESCryptoServiceProvider DCSP = new DESCryptoServiceProvider();
+            MemoryStream mStream = new MemoryStream();
+            CryptoStream cStream = new CryptoStream(mStream, DCSP.CreateDecryptor(rgbKey, Keys), CryptoStreamMode.Write);
+            cStream.Write(inputByteArray, 0, inputByteArray.Length);
+            cStream.FlushFinalBlock();
+            cStream.Dispose();
+            mStream.Dispose();
+            return Encoding.UTF8.GetString(mStream.ToArray());
+        }
+        catch
+        {
+            return decryptString;
+        }
+    }
+
+    #endregion
 }
--- Assets/test.cs
+++ Assets/test.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using UnityEngine;
 
 public class test : SingleMonoBase<test>
@@ -17,6 +20,14 @@ public class test : SingleMonoBase<test>
 
     void Start()
     {
+        string str = "大家好!23211231";
+        string key = "nm123456";
+        Debug.Log("加密前："+str);
+        string r= OtherTools.EncryptDES(str,key);
+        Debug.Log("加密后："+r);
+        r= OtherTools.DecryptDES(r,key);
+        Debug.Log("解密后："+r);
     }
+
     
 }
