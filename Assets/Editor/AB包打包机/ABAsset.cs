05fdd5ed48558cf2058063600108bc380842e3c0
qide <qide.yi@castbox.fm>
2022-03-11 11:08:33 +0800

    提交

--- Assets/Editor/AB包打包机/ABAsset.cs
+++ Assets/Editor/AB包打包机/ABAsset.cs
@@ -10,4 +10,58 @@ public class ABAsset
 {
     public string path;
     public Texture2D GeIcon => (Texture2D)AssetDatabase.GetCachedIcon(path);
+
+    public ABAsset(string path)
+    {
+        this.path = path;
+    }
+}
+
+public class Pkg
+{
+    public string pkgName="";
+    public List<string> dirs=new List<string>();
+    
+    private List<ABAsset> otherAllAB = new List<ABAsset>();
+
+    public void addDir(string path)
+    {
+        if (Directory.Exists(path)&&!dirs.Contains(path))
+        {
+            dirs.Add(path);
+        }
+    }
+
+    public void addABAsset(string path)
+    {
+        foreach (var item in  all())
+        {
+            if (item.path==path)
+            {
+                return;
+            }
+        }
+        otherAllAB.Add(new ABAsset(path));
+    }
+
+    public List<ABAsset> all()
+    {
+        var  all =new List<ABAsset>();
+        all.AddRange(otherAllAB);
+        foreach (var item in dirs)
+        {
+           var files= Directory.GetFiles(item);
+           foreach (var VARIABLE in files)
+           {
+               if (VARIABLE.Contains(".meta"))
+               {
+                   continue;
+               }
+               all.Add(new ABAsset(VARIABLE));
+           }
+        }
+
+        return all;
+    }
+
 }
--- Assets/Editor/AB包打包机/AssetBundleManager.cs
+++ Assets/Editor/AB包打包机/AssetBundleManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using Framework.Tools;
 using UnityEditor;
 using UnityEngine;
 
@@ -59,51 +60,132 @@ public class AssetBundleManager: BaseEditorWindow<AssetBundleManager>
             y += 25;
         }
         GUI.EndScrollView();*/
-        
-        
 
-        GUI.Box(new Rect(0, 55, 200, WindowHeight-55), "你哈");
-        pos = GUI.BeginScrollView(
-            new Rect(0,55, 200 ,WindowHeight-55),
-            pos,  
-            new Rect(0,55,185,pkgName.Count*25 >WindowHeight-55?pkgName.Count*25:WindowHeight-55),
-            false,
-            true);
-        
-        GUI.EndScrollView();
-        
-        GUI.Box(new Rect(210, 55, 200, WindowHeight-55), "你哈");
-        pos2 = GUI.BeginScrollView(
-            new Rect(210,55, 200 ,WindowHeight-55),
-            pos2,  
-            new Rect(210,55,185,pkgDir.Count*25 >WindowHeight-55?pkgDir.Count*25:WindowHeight-55),
-            false,
-            true);
-        
-        GUI.EndScrollView();
-        
-        
-        GUI.Box(new Rect(420, 55, WindowWidth-420, WindowHeight-55), "你哈");
-        pos3 = GUI.BeginScrollView(
-            new Rect(420,55, WindowWidth-420 ,WindowHeight-55),
-            pos3,  
-            new Rect(420,55,WindowWidth-420,files.Count*25 >WindowHeight-55?files.Count*25:WindowHeight-55),
-            false,
-            true);
-        
-        GUI.EndScrollView();
+
+       {
+           GUI.Box(box1, "");
+           pos = GUI.BeginScrollView(
+               box1,
+               pos,
+               new Rect(0, 55, 185, pkgName.Count * 25 > WindowHeight - 55 ? pkgName.Count * 25 : WindowHeight - 55),
+               false,
+               true);
+           if (GUI.Button(new Rect(box1.x + 0, box1.y, 185, 20), "添加AB包"))
+           {
+               pkgName.Add(new Pkg());
+           }
+           for (int i = 0; i < pkgName.Count; i++)
+           {
+               pkgName[i].pkgName=GUI.TextField(new Rect(box1.x + 0, box1.y + 20 + i * 25, 115, 20), pkgName[i].pkgName);
+               if (GUI.Button(new Rect(box1.x + 150, box1.y + 20 + i * 25, 35, 20), "删除"))
+               {
+                   pkgName.RemoveAt(i);
+               }
+               if (GUI.Button(new Rect(box1.x + 115, box1.y + 20 + i * 25, 35, 20), "选择"))
+               {
+                   selectPkg=pkgName[i];
+               }
+           }
+
+           GUI.EndScrollView();
+       }
+       if (selectPkg!=null)
+       {
+           {
+               GUI.Box(box2, "");
+               pos2 = GUI.BeginScrollView(
+                   box2,
+                   pos2,
+                   new Rect(210, 55, 185,
+                       pkgDir.Count * 25 > WindowHeight - 55 ? pkgDir.Count * 25 : WindowHeight - 55),
+                   false,
+                   true);
+               DragAndDropTool.CreationDragAndDropArea(
+                   box2,
+                   DragAction, "将文件夹拖到到这里");
+               for (int i = 0; i < selectPkg.dirs.Count; i++)
+               {
+                   GUI.Box(new Rect(box2.x, box2.y + i * 25, 20, 20),
+                       (Texture2D) AssetDatabase.GetCachedIcon(selectPkg.dirs[i]));
+                   GUI.TextField(new Rect(box2.x + 20, box2.y + i * 25, box2.width - 30, 20), selectPkg.dirs[i]);
+               }
+
+               GUI.EndScrollView();
+           }
+
+           {
+               GUI.Box(box3, "");
+               pos3 = GUI.BeginScrollView(
+                   box3,
+                   pos3,
+                   new Rect(420, 55, WindowWidth - 420,
+                       files.Count * 25 > WindowHeight - 55 ? files.Count * 25 : WindowHeight - 55),
+                   false,
+                   true);
+               DragAndDropTool.CreationDragAndDropArea(
+                   box3,
+                   DragAction2, "将文件拖到到这里");
+               var pkgs = selectPkg.all();
+               for (int i = 0; i < pkgs.Count; i++)
+               {
+                   GUI.Box(new Rect(box3.x, box3.y + i * 25, 20, 20), pkgs[i].GeIcon);
+                   GUI.TextField(new Rect(box3.x + 20, box3.y + i * 25, box3.width - 30, 20), pkgs[i].path);
+               }
+
+               GUI.EndScrollView();
+           }
+       }
     }
 
-    private List<string> pkgName=new List<string>();
+    private Pkg selectPkg;
+    private List<Pkg> pkgName=new List<Pkg>();
     private List<string> pkgDir = new List<string>();
     private List<string> files = new List<string>();
+    private Rect box1 => new Rect(0, 55, 200, WindowHeight - 55);
+    private Rect box2 => new Rect(210, 55, 200, WindowHeight-55);
+    private Rect box3 => new Rect(420, 55, WindowWidth - 420, WindowHeight - 55);
     private Vector2 pos;
     private Vector2 pos2;
     private Vector2 pos3;
 
+    private void OnEnable()
+    {
+        string path = Application.dataPath+"/Editor/AB包打包机/data.json";
+        try
+        {        
+            pkgName=SerializeTools.StringToObj<List<Pkg>>(FileTools.ReadFile(path));
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e);
+        }
+    }
+
+    private void Save()
+    {
+        string path = Application.dataPath+"/Editor/AB包打包机/data.json";
+        FileTools.WriteFile(path,SerializeTools.ObjToString(pkgName));
+    }
+
+    private void DragAction2(string[] strs)
+    {
+        foreach (var item in strs)
+        {
+            selectPkg.addABAsset(item);
+        }
+
+        Save();
+    }
+
     private void DragAction(string[] strs)
     {
-        str.AddRange(strs);
+        
+        foreach (var item in strs)
+        {
+            selectPkg.addDir(item);
+        }
+
+        Save();
     }
 
     private void BuildAllAssetBundles()
