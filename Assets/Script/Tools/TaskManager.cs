d2a2689333a952e997c605c626d4023ca4a9a14d
qide <qide.yi@castbox.fm>
2022-04-08 16:10:14 +0800

    优化

--- Assets/Script/Tools/TaskManager.cs
+++ Assets/Script/Tools/TaskManager.cs
@@ -10,20 +10,32 @@ namespace Framework.Tools
         /// 开始并执行一个异步任务
         /// </summary>
         /// <param name="executeTask">执行的任务</param>
+        /// <param name="overtime">任务超时时间，默认为0 -任务没有不会存在超时，但也可能永远不会返回  单位为毫秒</param>
         /// <param name="action">当任务完成后会调用 传入ture时任务就是完成的</param>
-        public static async void ExecuteTaskAsync(Action executeTask,Action<bool> action=null)
+        public static async void ExecuteTaskAsync(Action executeTask,int overtime=0,Action<bool> action=null)
         {
-            Task task = new Task(executeTask);
-            task.Start();
-            await task;
+            Task task = Task.Factory.StartNew(executeTask);
+            if (overtime==0)
+            {
+                await task;
+            }
+            else
+            {    
+                await Task.Delay(overtime);
+            }
             if (task.IsCompletedSuccessfully)
             {
                 action?.Invoke(true);
+                task.Dispose();
             }
             else
             {
-                Debug.LogError($"任务状态：{task.Status} 信息：{task.Exception}");
+                Debug.LogError($"任务状态：{task.Status}\n信息：{task.Exception}");
                 action?.Invoke(false);
+                if (task.IsFaulted || task.IsCanceled)
+                {
+                    task.Dispose();
+                }
             }
         }
         
@@ -31,22 +43,34 @@ namespace Framework.Tools
         /// 开始并执行一个异步任务
         /// </summary>
         /// <param name="executeTask">执行的任务</param>
+        /// <param name="overtime">任务超时时间，默认为0 -任务没有不会存在超时，但也可能永远不会返回   单位为毫秒</param>
         /// <param name="action">当任务完成后会调用 传入ture时任务就是完成的</param>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
-        public static async Task<T> ExecuteTaskAsync<T>(Func<T> executeTask,Action<bool,T> action=null)
+        public static async Task<T> ExecuteTaskAsync<T>(Func<T> executeTask,int overtime,Action<bool,T> action=null)
         {
-            Task<T> task = new Task<T>(executeTask);
-            task.Start();
-            await task;
-            if (task.IsCompletedSuccessfully)
+            Task<T> task =  Task<T>.Factory.StartNew(executeTask);
+            if (overtime==0)
             {
-                action?.Invoke(true,task.Result);
+                await task;
             }
             else
             {
-                Debug.LogError($"任务状态：{task.Status} 信息：{task.Exception}");
+                await Task.Delay(overtime);
+            }
+            if (task.IsCompletedSuccessfully)
+            {
+                action?.Invoke(true,task.Result);
+                task.Dispose();
+            }
+            else
+            {
+                Debug.LogError($"任务状态：{task.Status}\n信息：{task.Exception}");
                 action?.Invoke(false,default);
+                if (task.IsFaulted || task.IsCanceled)
+                {
+                    task.Dispose();
+                }
             }
             return task.Result;
         }
