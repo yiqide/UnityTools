dda09d7b12b2ce1a2d8bd988e7c4a533b0029525
qide <qide.yi@castbox.fm>
2022-05-18 18:06:55 +0800

    添加行为树的库

--- Assets/Script/Tools/EventManager.cs
+++ Assets/Script/Tools/EventManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using Framework.Setting;
 using Framework.SingleMone;
 using UnityEngine;
---
+++ Assets/Script/行为🌲树/BehaviorTreePerformer.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Framework.SingleMone;
+
+public class BehaviorTreePerformer : SingleMonoBase<BehaviorTreePerformer>
+{
+    //执行器
+    private void Start()
+    {
+        
+    }
+}
